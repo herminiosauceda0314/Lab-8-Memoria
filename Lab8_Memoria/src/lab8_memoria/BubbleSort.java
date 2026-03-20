700330a6b16bc19814308b6b38d166a56c7d3595
Gabriel <gpopoff504@gmail.com>
2026-03-20 09:35:02 -0600

    bubble sort y mergesort

--- Lab8_Memoria/src/lab8_memoria/BubbleSort.java
+++ Lab8_Memoria/src/lab8_memoria/BubbleSort.java
@@ -4,10 +4,63 @@
  */
 package lab8_memoria;
 
+import lab8_memoria.P4ListaEnlazada.Nodo;
 /**
  *
  * @author gpopo
  */
 public class BubbleSort {
     public enum Criterio {NOMBRE, TIPO}
+    
+    public static void ordenar(P4ListaEnlazada lista, Criterio criterio) {
+        if(lista == null || lista.estaVacia()) return;
+        
+        boolean huboCambio;
+        
+        do{
+            huboCambio = false;
+            Nodo actual = lista.getCabeza();
+            
+            while (actual != null && actual.siguente != null) {
+                Nodo siguente = actual.siguente;
+                
+                if(comparar(actual, siguente, criterio) > 0) {
+                    intercambiarArchivos(actual, siguente);
+                    huboCambio = true;
+                }
+                
+                actual = actual.siguente;
+            }
+        } while(huboCambio);
+    }
+    
+    private static int comparar (Nodo a, Nodo b, Criterio criterio) {
+        switch(criterio) {
+            
+            case NOMBRE: 
+                return a.getNombre().compareToIgnoreCase(b.getNombre());
+             
+            case TIPO: 
+                int cmpTipo = a.getTipo().compareToIgnoreCase(b.getTipo());
+                return cmpTipo != 0
+                        ? cmpTipo
+                        : a.getNombre().compareToIgnoreCase(b.getNombre());
+            default:
+                return 0;
+        }
+    }
+    
+    private static void intercambiarArchivos(Nodo a, Nodo b) {
+        java.io.File temporal = a.archivo;
+        a.archivo = b.archivo;
+        b.archivo = temporal;
+    }
+    
+    public static void ordenarPorrNombre(P4ListaEnlazada lista) {
+        ordenar(lista, Criterio.NOMBRE);
+    }
+    
+    public static void ordenarPorTipo(P4ListaEnlazada lista){
+        ordenar(lista, Criterio.TIPO);
+    }
 }
--- Lab8_Memoria/src/lab8_memoria/MergeSort.java
+++ Lab8_Memoria/src/lab8_memoria/MergeSort.java
@@ -9,5 +9,77 @@ package lab8_memoria;
  * @author gpopo
  */
 public class MergeSort {
+    public enum Criterio { FECHA, TAMANIO }
     
+    public static void ordenar(P4ListaEnlazada lista, Criterio criterio) {
+        if(lista == null || lista.estaVacia()) return;
+        
+        Nodo nuevaCabeza = ordenarRecursivo(lista.getCabeza(), criterio);
+        lista.setCabeza(nuevaCabeza);
+    }
+    
+    private static Nodo ordenarRecursivo(Nodo cabeza, Criterio criterio) {
+        if(cabeza == null || cabeza.siguente == null) return cabeza;
+        
+        Nodo medio = encontrarMedio(cabeza);
+        Nodo segundaMitad = medio.siguente;
+        medio.siguente = null;
+        
+        Nodo izquierda = ordenarRecursivo(cabeza, criterio);
+        Nodo derecha = ordenarRecursivo(segundaMitad, criterio);
+        
+        return mezclar(izquierda, derecha, criterio);
+    }
+    
+    private static Nodo encontrarMedio(Nodo cabeza) {
+        if (cabeza == null) return null;
+        
+        Nodo tortuga = cabeza;
+        Nodo liebre = cabeza.siguente;
+        
+        while (liebre != null && liebre.siguente != null) {
+            tortuga = tortuga.siguente;
+            liebre = liebre.siguente.siguente;
+        }
+        return tortuga;
+    }
+    
+    private static Nodo mezclar(Nodo izquierda, Nodo derecha, Criterio criterio) {
+        Nodo dummy = new Nodo(new java.io.File("."));
+        Nodo cola = dummy;
+        
+        while (izquierda != null && derecha != null) {
+            if (comparar(izquierda, derecha, criterio) <= 0){
+                cola.siguente = izquierda;
+                izquierda = izquierda.siguente;
+            } else{
+                cola.siguente = derecha;
+                derecha = derecha.siguente;
+            }
+            cola = cola.siguente;
+        }
+        
+        cola.siguente = (izquierda != null) ? izquierda : derecha;
+        return dummy.siguente;
+    }
+    
+    private static int comparar(Nodo a, Nodo b, Criterio criterio) {
+        switch(criterio) {
+            case FECHA:
+                return Long.compare(b.getFecha(), a.getFecha());
+            case TAMANIO: 
+                return Long.compare(a.getTamanio(), b.getTamanio());
+                
+            default: 
+                return 0;
+        }
+    }
+    
+    public static void ordenarPorFecha(P4ListaEnlazada lista) {
+        ordenar(lista, Criterio.FECHA);
+    }
+    
+    public static void ordenarPorTamanio(P4ListaEnlazada lista) {
+        ordenar(lista, Criterio.TAMANIO);
+    }
 }
